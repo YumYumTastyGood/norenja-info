21ed19f5ee9fc87ac8d4e210a5666aa521193833
myong-hoon <92171034+myong-hoon@users.noreply.github.com>
2021-11-07 14:18:51 +0900

    실시간 현위치 및 리스트안에 딕셔너리 형태의 정보를 지도에 표시([ { title:  , content: ,latlng:}]

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def Index():
+    return render_template('index.html')
+    
+@app.route('/location')
+def Index1():
+    return render_template('location.html')
+
+if __name__=='__main__':
+    app.run()
